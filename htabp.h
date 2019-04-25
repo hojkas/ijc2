3faf7470a85d43cb4f50bc51388437328bf0c57b
hojkas <ivka.strnadova@gmail.com>
2019-04-25 17:19:49 +0000

    update

---
+++ htabp.h
@@ -0,0 +1,14 @@
+#include "htab.h"
+/*
+struct {
+	int size;
+	int arr_size;
+	struct htab_item_t** htab_bucket;
+} htab_t;
+
+typedef struct {
+	char** key;
+	int data;
+	struct htab_item_t* next;
+} htab_item_t;
+*/
